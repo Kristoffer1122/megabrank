b39a2fd9d16b940eedd7ffbc024eacd1c902cc39
Kristoffer 1122 <krkja001@osloskolen.no>
2026-01-24 16:55:06 +0100

    gravity, healthbars

--- src/entities/enemies.c
+++ src/entities/enemies.c
@@ -1,5 +1,6 @@
 #include "enemies.h"
 #include "../core/time.h"
+#include "../game/camera.h"
 #include <raylib.h>
 #include <raymath.h>
 #include <stdio.h>
@@ -20,6 +21,7 @@ void init_enemy(EnemyList *enemy_list, EnemyType enemy_type) {
     for (int x = 0; x < ENEMY_MAX_COUNT; x++) {
       enemy_list->enemies[x].enemy_type = ENEMY_TYPE_ZOMBIE;
       enemy_list->enemies[x].health = 100;
+      enemy_list->enemies[x].health_bar_offset = (Vector3){0.0f, 1.5f, 0.0f};
       enemy_list->enemies[x].attack = 10;
       enemy_list->enemies[x].defense = 5;
       snprintf(enemy_list->enemies[x].name, sizeof(enemy_list->enemies[x].name),
@@ -30,6 +32,7 @@ void init_enemy(EnemyList *enemy_list, EnemyType enemy_type) {
     for (int x = 0; x < ENEMY_MAX_COUNT; x++) {
       enemy_list->enemies[x].enemy_type = ENEMY_TYPE_ALIEN;
       enemy_list->enemies[x].health = 80;
+      enemy_list->enemies[x].health_bar_offset = (Vector3){0.0f, 1.5f, 0.0f};
       enemy_list->enemies[x].attack = 15;
       enemy_list->enemies[x].defense = 3;
       snprintf(enemy_list->enemies[x].name, sizeof(enemy_list->enemies[x].name),
@@ -40,6 +43,7 @@ void init_enemy(EnemyList *enemy_list, EnemyType enemy_type) {
     for (int x = 0; x < ENEMY_MAX_COUNT; x++) {
       enemy_list->enemies[x].enemy_type = ENEMY_TYPE_ROBOT;
       enemy_list->enemies[x].health = 120;
+      enemy_list->enemies[x].health_bar_offset = (Vector3){0.0f, 1.5f, 0.0f};
       enemy_list->enemies[x].attack = 8;
       enemy_list->enemies[x].defense = 10;
       snprintf(enemy_list->enemies[x].name, sizeof(enemy_list->enemies[x].name),
@@ -52,8 +56,16 @@ void init_enemy(EnemyList *enemy_list, EnemyType enemy_type) {
 void update_enemies(EnemyList *enemy_list, Vector3 target) {
   for (int i = 0; i < enemy_list->enemy_count; i++)
     if (enemy_list->enemies[i].alive == true)
+
       // make enemy move towards player
+      // NOTE: we can later add pathfinding here
+      // but for now just direct movement
+
       if (Vector3Distance(target, enemy_list->enemies[i].position) > 0.0f) {
+
+        // Keep enemies on ground level
+        enemy_list->enemies[i].position.y = 0.0f;
+
         Vector3 direction =
             Vector3Subtract(target, enemy_list->enemies[i].position);
         float length = Vector3Length(direction);
@@ -85,6 +97,23 @@ void draw_enemies(EnemyList *enemy_list) {
         DrawSphere(enemy_list->enemies[x].position, 0.5f, GRAY);
         break;
       }
+
+       // makeshift healthbar
+       DrawCube(enemy_list->enemies[x].health_bar_offset, 1.5f, 0.4f, 0.001f, WHITE);
+
+       DrawCube(Vector3Add(enemy_list->enemies[x].position, 
+                           (Vector3){enemy_list->enemies[x].health_bar_offset.x - 0.12f,
+                           enemy_list->enemies[x].health_bar_offset.y,
+                           enemy_list->enemies[x].health_bar_offset.y + 0.01f}),
+                           0.012f * enemy_list->enemies[x].health, 0.3f, 0.001f, RED);
+
+       DrawCube(Vector3Add(enemy_list->enemies[x].position, 
+                           (Vector3){enemy_list->enemies[x].health_bar_offset.x - 0.12f,
+                           enemy_list->enemies[x].health_bar_offset.y,
+                           enemy_list->enemies[x].health_bar_offset.y - 0.01f}),
+                           1.2f, 0.3f, 0.001f, RAYWHITE);
+
+
     } else {
       // enemy dead
       DrawSphere(enemy_list->enemies[x].position, 0.5f, RED);
