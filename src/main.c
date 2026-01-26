fc461dcfb2918e8b9512ef9815bd6b648b8bc72e
Kristoffer 1122 <krkja001@osloskolen.no>
2026-01-26 20:57:21 +0100

    track level and exp

--- src/main.c
+++ src/main.c
@@ -67,6 +67,10 @@ int main() {
     DrawText("Use WASD to move, SPACE to jump", 10, 10, 20, DARKGRAY);
     DrawText(TextFormat("Enemies killed:💀 %d", player.kill_count), 10, 40, 20, DARKGRAY);
 
+    DrawText(TextFormat("Level: %d, EXP: %d", player.level, player.experience), 10, 70, 20, DARKGRAY);
+
+
+
     EndDrawing();
   }
 
